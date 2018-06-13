769a96941e39977ddb59b1058ebdcbe3a2a75a1d
root <root@LAPTOP-QNSG3TAU.localdomain>
2018-06-12 21:19:15 -0400

    Initial commit

---
+++ Starter_Code/assign-3-boggle/assign-3-boggle/src/boggle.cpp
@@ -0,0 +1,102 @@
+/**
+ * File: boggle.cpp
+ * ----------------
+ * Implements the game of Boggle.
+ */
+ 
+#include <cctype>
+#include <iostream>
+#include "console.h"
+#include "gboggle.h"
+#include "simpio.h"
+using namespace std;
+
+static const int kBoggleWindowWidth = 650;
+static const int kBoggleWindowHeight = 350;
+static const string kStandardCubes[16] = {
+   "AAEEGN", "ABBJOO", "ACHOPS", "AFFKPS",
+   "AOOTTW", "CIMOTU", "DEILRX", "DELRVY",
+   "DISTTY", "EEGHNW", "EEINSU", "EHRTVW",
+   "EIOSST", "ELRTTY", "HIMNQU", "HLNNRZ"
+};
+
+static const string kBigBoggleCubes[25] = {
+   "AAAFRS", "AAEEEE", "AAFIRS", "ADENNN", "AEEEEM",
+   "AEEGMU", "AEGMNN", "AFIRSY", "BJKQXZ", "CCNSTW",
+   "CEIILT", "CEILPT", "CEIPST", "DDLNOR", "DDHNOT",
+   "DHHLOR", "DHLNOR", "EIIITT", "EMOTTT", "ENSSSU",
+   "FIPRSY", "GORRVW", "HIPRRY", "NOOTUW", "OOOTTU"
+};
+
+static const int kMinLength = 4;
+static const double kHighlightDelay = 350;
+
+/**
+ * Function: welcome
+ * Usage: welcome();
+ * -----------------
+ * Print out a cheery welcome message.
+ */
+static void welcome() {
+    cout << "Welcome!  You're about to play an intense game ";
+    cout << "of mind-numbing Boggle.  The good news is that ";
+    cout << "you might improve your vocabulary a bit.  The ";
+    cout << "bad news is that you're probably going to lose ";
+    cout << "miserably to this little dictionary-toting hunk ";
+    cout << "of silicon.  If only YOU had a gig of RAM..." << endl;
+    cout << endl;
+}
+
+/**
+ * Function: giveInstructions
+ * Usage: giveInstructions();
+ * --------------------------
+ * Print out the instructions for the user.
+ */
+static void giveInstructions() {
+    cout << "The boggle board is a grid onto which I ";
+    cout << "I will randomly distribute cubes. These ";
+    cout << "6-sided cubes have letters rather than ";
+    cout << "numbers on the faces, creating a grid of ";
+    cout << "letters on which you try to form words. ";
+    cout << "You go first, entering all the words you can ";
+    cout << "find that are formed by tracing adjoining ";
+    cout << "letters. Two letters adjoin if they are next ";
+    cout << "to each other horizontally, vertically, or ";
+    cout << "diagonally. A letter can only be used once ";
+    cout << "in each word. Words must be at least four ";
+    cout << "letters long and can be counted only once. ";
+    cout << "You score points based on word length: a ";
+    cout << "4-letter word is worth 1 point, 5-letters ";
+    cout << "earn 2 points, and so on. After your puny ";
+    cout << "brain is exhausted, I, the supercomputer, ";
+    cout << "will find all the remaining words and double ";
+    cout << "or triple your paltry score." << endl;
+    cout << endl;
+    cout << "Hit return when you're ready...";
+    getLine(); // ignore return value
+}
+
+/**
+ * Function: playBoggle
+ * --------------------
+ * Manages all details needed for the user to play one
+ * or more games of Boggle.
+ */
+static void playBoggle() {
+    // This is where you'll start adding code
+}
+
+/**
+ * Function: main
+ * --------------
+ * Serves as the entry point to the entire program.
+ */
+int main() {
+    GWindow gw(kBoggleWindowWidth, kBoggleWindowHeight);
+    initGBoggle(gw);
+    welcome();
+    if (getYesOrNo("Do you need instructions?")) giveInstructions();
+    playBoggle();
+    return 0;
+}
